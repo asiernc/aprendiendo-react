df2d7ffbd1a80ff373b8d42296d7b5e0aa173280
asiernc <asierncara@gmail.com>
2023-07-24 10:45:57 +0200

    sd

--- projects/06-practica-useState-y-useEffect/src/App.jsx
+++ projects/06-practica-useState-y-useEffect/src/App.jsx
@@ -72,18 +72,7 @@ function App() {
     setStartTimer(false)
   }
 
-  // EXERCISE 3
-
-/* Declara una variable de estado llamada users utilizando el hook useState e inicialízala con un array vacío.
-Crea una función addUser que reciba el nombre y el correo electrónico de un usuario como parámetros y agregue un nuevo
- usuario al estado users. El nuevo usuario debe ser un objeto con las propiedades id (generado automáticamente), name 
- (nombre del usuario) y email (correo electrónico del usuario).
-Renderiza los elementos del componente:
-Agrega un formulario con campos de entrada de texto para ingresar el nombre y el correo electrónico de un nuevo usuario.
-Agrega un botón "Agregar Usuario" que permita al usuario agregar un nuevo usuario a la lista.
-Muestra la lista de usuarios en una tabla. Cada usuario debe mostrarse en una fila de la tabla con las columnas de "Nombre" y "Correo electrónico".
-Utiliza el hook useEffect para mostrar un mensaje cuando se agregue un nuevo usuario. El mensaje debe aparecer durante
- 3 segundos y luego desaparecer. */
+  // EXERCISE 3git 
 
   const [users, setUsers] = useState([])
   const [name, setName] = useState('')
